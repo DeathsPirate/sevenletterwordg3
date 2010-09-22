2e397cbc957c84d1978ce74472f11797276016b9
DavidSnooker@gmail.com <DavidSnooker@gmail.com@bef5b74f-c185-6451-0687-19c43317e5a6>
2010-09-22 00:07:18 +0000

    Redesign the calculation of bid amount

--- trunk/f10_4444_seven/src/seven/f10/g3/History.java
+++ trunk/f10_4444_seven/src/seven/f10/g3/History.java
@@ -6,23 +6,75 @@ import java.util.ListIterator;
 
 import org.apache.log4j.Logger;
 
+import seven.ui.Letter;
 import seven.ui.PlayerBids;
 
 public class History
 {
 	private static ArrayList<BidLog> bidLogList=new ArrayList<BidLog>();
 	private static final Logger l=Logger.getLogger(History.class);
-	private static double topWeightAtStdDev=0.3;
+	private static double meanAdjustSum=0;
+	private static double[] marketValue={9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 
+		1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1};
 	
-	public static double adjust(String b, ArrayList<PlayerBids> cachedBids, int ourID)
+	public static int adjust(String bidStrategy, Letter bidLetter,  
+		ArrayList<PlayerBids> cachedBids, int ourID)
 	{
+		double meanAdjust=0;
+		double bid=marketValue[bidLetter.getAlphabet()-'A'];
+		
+		// first round
 		if (cachedBids.size()==0)
-			return 0;
+			return (int)(bid+0.5);
+		
 		int lastRound=cachedBids.size()-1;
-		PlayerBids bid=cachedBids.get(lastRound);
+		PlayerBids lastBids=cachedBids.get(lastRound);
+		int ourLastBid=lastBids.getBidvalues().get(ourID);
+		
+		// mean adjust
+		double radiusMean=setRadiusMean(lastBids, ourID);
+		if (radiusMean!=-1)
+			meanAdjust=0.5*(radiusMean-ourLastBid);
+		
+		// store last letter market value
+		Letter lastLetter=lastBids.getTargetLetter();
+		int lastLetterIndex=lastLetter.getAlphabet()-'A';
+		if (radiusMean!=-1)
+			marketValue[lastLetterIndex]=0.6*radiusMean+0.4*marketValue[lastLetterIndex];
+		
+		// bid according to market value
+		bid=marketValue[bidLetter.getAlphabet()-'A']; 
+		
+		// strategy		
+		if (bidStrategy.equals("M"))
+		{
+			meanAdjustSum+=meanAdjust;
+			bid+=meanAdjustSum;
+			l.error("bid M: meadAdjustSum="+meanAdjustSum);
+		}
+		else if (bidStrategy.equals("L"))
+		{
+			bid*=0.7;
+		}
+		else if (bidStrategy.equals("H"))
+		{
+			bid*=1.2;
+		}
+		else if (bidStrategy.equals("7th"))
+		{
+			// Do not update meanAdjustSum
+			double riskRate=1.5;
+			bid=riskRate*(bidLetter.getValue()+50);
+			//TODO if > current money
+		}
+		return (int)(bid+0.5);
+	}
+	
+	public static double setRadiusMean(PlayerBids lastBids, int ourID)
+	{
 		int sum=0;
 		int top=0;
-		ListIterator<Integer> it=bid.getBidvalues().listIterator();
+		ListIterator<Integer> it=lastBids.getBidvalues().listIterator();
 		while (it.hasNext())
 		{
 			int temp=it.next();
@@ -30,9 +82,8 @@ public class History
 				top=temp;
 			sum+=temp;
 		}
-		double mean=1.000*sum/bid.getBidvalues().size();
-		double median=getMedian(bid.getBidvalues());
-		
+		double mean=1.000*sum/lastBids.getBidvalues().size();
+		double median=getMedian(lastBids.getBidvalues());		
 		
 		//store parameters in BidLog		
 		BidLog bidLog=new BidLog();
@@ -42,6 +93,7 @@ public class History
 		bidLog.setTop(top);
 		
 		//diffMean and Standard Deviation
+		/*
 		ArrayList<Double> diffMean=new ArrayList<Double>();
 		bidLog.setDiffMean(diffMean);
 		it=bid.getBidvalues().listIterator();
@@ -55,26 +107,14 @@ public class History
 		}
 		double stdDev=Math.sqrt((diffSum/(diffMean.size()-1)));
 		bidLog.setStdDev(stdDev);
-		double devMean=getDevMean(bid.getBidvalues(), mean, stdDev);
+		double devMean=getDevMean(bid.getBidvalues(), mean, stdDev, devRange);
 		bidLog.setDevMean(devMean);
-		
+		*/
+		double radiusMean=getRadiusMean(lastBids.getBidvalues(), mean);
+		bidLog.setRadiusMean(radiusMean);
 		bidLogList.add(bidLog);
 		
-		double topWeight;
-		double topDevMeanDiff=top-devMean;
-		double topWeightC=topWeightAtStdDev/(1-topWeightAtStdDev)*stdDev;
-		if (topDevMeanDiff==0)
-			topWeight=1;
-		else
-			topWeight=topWeightC/(topWeightC+topDevMeanDiff);
-		
-		double adjust=top*topWeight+devMean*(1-topWeight)-bid.getBidvalues().get(ourID);
-		/*l.trace("Top="+top+",StdDev="+stdDev+",Mean="+mean+",DevMean="+devMean
-			+",topWeight="+topWeight+",Adjust="+adjust);*/
-		if (adjust>=0)
-			return adjust+0.5;
-		else
-			return adjust-0.5;
+		return radiusMean;
 	}
 	
 	public static double getMedian(ArrayList<Integer> arrayList)
@@ -88,10 +128,10 @@ public class History
 			return list.get(len/2);
 	}
 	
-	public static double getDevMean(ArrayList<Integer> arrayList, double mean, double stdDev)
+	public static double getDevMean(ArrayList<Integer> arrayList, double mean, double stdDev, double range)
 	{
-		double min=mean-stdDev;
-		double max=mean+stdDev;
+		double min=mean-stdDev*range;
+		double max=mean+stdDev*range;
 		ListIterator<Integer> it=arrayList.listIterator();
 		double sum=0;
 		double count=0;
@@ -99,16 +139,42 @@ public class History
 		while (it.hasNext())
 		{
 			val=it.next();
-			l.trace("getDevMean() val="+val);
 			if (val>=min && val<=max)
 			{
 				sum+=val;
 				count++;
 			}
 		}
-		l.trace("getDevMean() sum="+sum);
 		if (count==0)
-			return mean;
+			return -1;
+		return 1.000*sum/count;
+	}
+	
+	public static int getRadius(double mean)
+	{
+		return (int)(Math.sqrt(mean)+0.5);
+	}
+	
+	public static double getRadiusMean(ArrayList<Integer> arrayList, double mean)
+	{
+		int radius=getRadius(mean);
+		double min=mean-radius;
+		double max=mean+radius;
+		ListIterator<Integer> it=arrayList.listIterator();
+		double sum=0;
+		double count=0;
+		double val=0;
+		while (it.hasNext())
+		{
+			val=it.next();
+			if (val>=min && val<=max)
+			{
+				sum+=val;
+				count++;
+			}
+		}
+		if (count==0)
+			return -1;
 		return 1.000*sum/count;
 	}
 }
