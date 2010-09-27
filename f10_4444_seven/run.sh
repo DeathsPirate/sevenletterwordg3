2f12e0972c9b0c8db8790f71fc49b9a91fb0a7dc
lep2128 <lep2128@gmail.com>
2010-09-27 19:26:12 +0000

    Added in other teams players so that we can run mock tournaments

--- f10_4444_seven/run.sh
+++ f10_4444_seven/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp bin:bin/seven/g2/log4j-1.2.15.jar seven.ui.GameEngine $1 $2 $3 $4 
+java -cp bin:bin/seven/g2/log4j-1.2.15.jar seven.ui.GameEngine "seven.g0.StingyPlayer seven.g0.LessStingyPlayer" 2 50 testOut
